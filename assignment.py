013c594d2290ee6c58b7417da4de30ce3f6fb145
lizbaraza93-web <lizbaraza93@gmail.com>
2026-03-11 17:59:02 +0300

    Add assignment.py with questions and answers

---
+++ assignment.py
@@ -0,0 +1,27 @@
+#Question 4
+print("Hello AI Students")
+
+#Question 5
+print("Name":John)
+print("Age":20)
+print("City":Nairobi)
+
+#Question 6
+languages = ["Python", "Java", "C++", "JavaScript", "Go"]
+print(languages[0])
+
+#Question 7
+student = {
+"name": "John",
+"age": 21,
+"course": "AI"
+}
+
+#Question 8
+A list is a collection of items that can carry different data types at the same time.
+A dictionary stores data using key-value pairs.
+
+
+
+
+
